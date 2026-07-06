ea46b8c72d5d49ea24a1499eb027a524ef93d80f
esteban <juanestebananguloperez75@gmail.com>
2026-07-06 14:56:29 -0500

    chore: agregar archivo de inicialización del paquete modelos

---
+++ app/modelos/__init__.py
@@ -0,0 +1,4 @@
+# Inicialización del paquete modelos
+from .Clientes import Cliente, ClienteCrear
+from .Facturas import Factura, FacturaCrear
+from .Transacciones import Transaccion, TransaccionCrear
